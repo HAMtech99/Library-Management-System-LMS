4c4d75f45504a11f0f96439c4191c3a0b6a2a2d1
HAMtech99 <muhammad.hamdan26@gmail.com>
2026-06-20 22:37:49 -0400

    Comments to main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
+
+    // Application entry point - launches the Library Management System
     public static void main(String[] args) {
+
+        // Print welcome message to greet the user on startup
         System.out.println("Welcome to the Library Management System!");
+
+        // Create the LibrarySystem to hold all patron data in memory
         LibrarySystem system = new LibrarySystem();
+
+        // Create the MenuHandler and pass the system to it
+        // MenuHandler owns all user interaction from this point forward
         MenuHandler menuHandler = new MenuHandler(system);
+
+        // Start the menu loop - program runs until user selects Exit
         menuHandler.run();
     }
 }
